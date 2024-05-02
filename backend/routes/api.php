5cf921795291a2328bada3d3542f83adf7daf76a
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-05-02 15:33:38 +0200

    rotte, api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// API per ottenere tutti gli utenti con i relativi post
+Route::get('/users', function () {
+    return User::with('posts')->get();
+});
+
+// API per ottenere un singolo utente con i relativi post
+Route::get('/users/{id}', function ($id) {
+    return User::with('posts')->findOrFail($id);
+});
+
+// API per creare un nuovo post per un utente specifico
+Route::post('/users/{userId}/posts', function (Request $request, $userId) {
+    $user = User::findOrFail($userId);
+    $post = new Post([
+        'title' => $request->input('title'),
+        'content' => $request->input('content')
+    ]);
+    $user->posts()->save($post);
+    return $post;
+});
+
+// API per aggiornare un post esistente di un utente
+Route::put('/users/{userId}/posts/{postId}', function (Request $request, $userId, $postId) {
+    $post = Post::findOrFail($postId);
+    $post->update($request->only(['title', 'content']));
+    return $post;
+});
+
+// API per eliminare un post di un utente
+Route::delete('/users/{userId}/posts/{postId}', function ($userId, $postId) {
+    Post::where('user_id', $userId)->findOrFail($postId)->delete();
+    return 204;
 });
